95568b691731af903c0332425540619eab7187c4
ftnext <takuyafjp+develop@gmail.com>
2018-11-10 16:29:27 +0900

    [feat] ログインユーザがログアウトできるように変更

--- mysite/urls.py
+++ mysite/urls.py
@@ -21,5 +21,6 @@ from django.contrib.auth import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/login/', views.login, name='login'),
+    path('accounts/logout/', views.logout, name='logout', kwargs={'next_page': '/'}),
     path('', include('blog.urls')),
 ]
