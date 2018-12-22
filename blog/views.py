e07321784c4afd4459f4df653af8a5e4c81e23bb
ftnext <takuyafjp+develop@gmail.com>
2018-12-22 11:48:45 +0900

    [feat] コメントの承認機能・削除機能を実装

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
+from .models import Comment
 from .models import Post
 from .forms import CommentForm
 from .forms import PostForm
@@ -72,3 +73,15 @@ def add_comment_to_post(request, pk):
     else:
         form = CommentForm()
     return render(request, 'blog/add_comment_to_post.html', {'form': form})
+
+@login_required
+def comment_approve(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+    return redirect('post_detail', pk=comment.post.pk)
+
+@login_required
+def comment_remove(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.delete()
+    return redirect('post_detail', pk=comment.post.pk)
