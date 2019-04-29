36e0443450266ff2339a28b94b6eebc6fdce16f1
ftnext <takuyafjp+develop@gmail.com>
2019-04-29 23:13:50 +0900

    [feat] ユーザを読者グループに紐づけてフォームから登録できるように変更
    * 登録後はログインされるようにした（デフォルトではログインされない）
    * ref: https://qiita.com/hayata-yamamoto/items/00072091caa5921fc819#comment-ea0e8b2e32d32feb5e38
    * ref2: https://torina.top/detail/296/

--- user/views.py
+++ user/views.py
@@ -1,4 +1,7 @@
+from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import Group
+from django.http import HttpResponseRedirect
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
@@ -7,3 +10,14 @@ class RegisterView(CreateView):
     template_name = 'user/register.html'
     form_class = UserCreationForm
     success_url = reverse_lazy('post_list')
+
+    def form_valid(self, form):
+        """
+        フォーム入力に問題がなければ、userを保存した上で読者のグループに追加。
+        ログイン状態にして、success_urlに遷移させる
+        """
+        user = form.save()
+        group = Group.objects.get(name='ReaderUser')
+        user.groups.add(group)
+        login(self.request, user)
+        return HttpResponseRedirect(self.success_url)
